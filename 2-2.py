66344764ebd13e601ddba5db24da078f214d3ab0
kyoong <barkhye@gmail.com>
2023-09-14 23:11:13 +0900

    add file mod

---
+++ 2-2.py
@@ -0,0 +1,31 @@
+""" my_var = 10
+print(my_var)
+
+my_list = [1, 2, 3]
+print(*my_list) """
+
+""" my_list = [10, 3, 8, 9, 42, 8, "hello"]
+print(my_list)
+
+print(my_list[3]) """
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list[2:5])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list[0:3])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list[0:5])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list[0:2])
+
+my_list = [10, 3, 8, 9, 42, "hello"]
+
+print(my_list[1:3])
+
